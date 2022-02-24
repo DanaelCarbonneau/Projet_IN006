a7966977310e0c55cb942cc52a1a9a65b225a65f
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-02-24 18:46:42 +0100

    ajout du rajout

--- Partie_1/tests_primalite.h
+++ Partie_1/tests_primalite.h
@@ -7,11 +7,22 @@
 /*Fonction calculant de manière naïve si p est est premier (en testant s'il est divisible par tous les entiers inférieurs)*/
 int is_prime_naive(long p);
 
+/*Fonction calculant (a^m)%n de manière naïve*/
+long modpow_naive(long a, long m, long n);
+
+/*Fonction calculant (a^m)%n*/
+int modpow(long a, long m, long n);
+
 /*retourne si a est un témoin de p (test de Miller)*/
 int witness ( long a , long b , long d , long p );
 
 /*retourne si p est un nombre premier (test de Miller)*/
 int is_prime_miller ( long p , int k );
 
+/*Fonction permettant de se donner 2^pow*/
+long pow_2(int pow);
+
+/*Fonction tentant de trouver un nombre premier d'une certaine taille*/
+long random_prime_number(int low_size, int up_size, int k);
 
 #endif
