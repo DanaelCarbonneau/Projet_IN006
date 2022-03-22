18809b7a37403f38bcd805e9c10be13a7cdbc8c2
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-03-22 19:18:21 +0100

    Update test.c

--- Partie_3/test.c
+++ Partie_3/test.c
@@ -3,9 +3,16 @@
 
 int main(){
 
-    CellKey* chaine = read_public_keys("../Partie_2/candidates.txt");
+    CellKey* chaine_cle = read_public_keys("../Partie_2/candidates.txt");
 
-    print_list_keys(chaine);
+    print_list_keys(chaine_cle);
 
-    delete_list_keys(chaine);
+    delete_list_keys(chaine_cle);
+    
+    CellProtected* chaine_pr = read_protected();
+    
+    print_list_protected(chaine_pr);
+    
+    delete_list_protected(chaine_pr);
+    
 }
