1f1bf82afee96fcc38b41b315ab351116e31a835
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2022-03-22 18:13:59 +0100

    bruh c'est littéralement la moitié

--- Partie_3/chaine_cle.c
+++ Partie_3/chaine_cle.c
@@ -1,8 +1,43 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+#include<stdio.h>
 
 #include"chaine_cle.h"
 
+
+CellKey* create_cell_key(Key* key){
+	CellKey* nouv = (CellKey*)malloc(sizeof(CellKey));
+	if (nouv == NULL){
+		printf("Erreur à l'allocation.");
+	}
+	nouv->data = key;
+	nouv->next = NULL;
+	return nouv;
+}
+
+CellKey* ajoutEnTete(Key* key, CellKey* liste){
+	CellKey* nouv = create_cell_key(key);
+	nouv->next = liste;
+	liste = nouv;
+}
+
+CellKey* read_public_keys(char* nom_fichier){
+	FILE* f = fopen(nom_fichier,"r");
+	if (f==NULL){
+		printf("Erreur d'ouverture du fichier.");
+	}
+	CellKey* liste = NULL;
+	char ligne[256];
+	char key_char[128];
+	char bin[128];
+	while (fgets(ligne, 256,f)!=NULL){
+		if (sscanf(ligne, "%s : %s, %s",bin,key_char,bin) == 3){
+			CellKey* nouv = create_cell_key(str_to_key(key_char));
+			liste = ajoutEnTete(nouv);
+		}else{
+			printf("Erreur de formatage!");
+		}
+	}
+	return liste;
+}
 
 
 void print_list_keys(CellKey* LCK){
@@ -21,8 +56,6 @@ void delete_cell_key(CellKey* c){
 }
 
 
-
-/*Fonction qui supprime une liste chainée de clées LCK*/
 void delete_list_keys(CellKey*LCK){
     CellKey* courant = LCK;
     while (courant){
@@ -31,44 +64,3 @@ void delete_list_keys(CellKey*LCK){
         delete_cell_key(courant);
     }    
 }
-=======
-=======
-#include "chaine_cle.h"
-
->>>>>>> 397424d0cb0ab0a40115f9205f0f6876fe1b21c0
-CellKey* create_cell_key(Key* key){
-	CellKey* nouv = (CellKey*)malloc(sizeof(CellKey));
-	if (nouv == NULL){
-		printf("Erreur à l'allocation.");
-	}
-	nouv->data = key;
-	nouv->next = NULL;
-	return nouv;
-}
-
-CellKey* ajoutEnTete(Key* key, CellKey* liste){
-	CellKey* nouv = create_cell_key(key);
-	nouv->next = liste;
-	liste = nouv;
-}
-
-CellKey* read_public_keys(char* nom_fichier){
-	File* f = fopen(nom_fichier,"r");
-	if (f==NULL){
-		printf("Erreur d'ouverture du fichier.");
-	}
-	CellKey* liste = NULL;
-	char ligne[256];
-	char key_char[128];
-	char bin[128];
-	while (fgets(ligne, 256,f)!=NULL){
-		if (sscanf(ligne, "%s : %s, %s",bin,key_char,bin) == 3){
-			CellKey* nouv = create_cell_key(str_to_key(key_char));
-			liste = ajoutEnTete(nouv);
-		}else{
-			printf("Erreur de formatage!");
-		}
-	}
-	return liste;
-}
->>>>>>> d9589ce8cb7abf462abd06e6234054866a1a4366
--- Partie_3/chaine_cle.h
+++ Partie_3/chaine_cle.h
@@ -8,13 +8,23 @@
 #include"../Partie_2/gestion_clef.h"
 
 
-
-
 //Structure utilisée
 typedef struct cellKey {
     Key * data ;
     struct cellKey * next ;
 } CellKey ;
+
+
+/*Fonction créant une cellule de la liste chaînée à partir d'une clé Key*/
+CellKey* create_cell_key(Key* key);
+
+/*Fonction ajoutant une cellule contenant la clé key en tête de la liste chainée liste*/
+CellKey* ajoutEnTete(Key* key, CellKey* liste);
+
+
+/*Fonction permettant de lire dans un fichier des clés et d'en créer une liste chainée*/
+CellKey* read_public_keys(char* nom_fichier);
+
 
 /* ́fonction permettant d’afficher une liste chainée de cles LCK*/
 void print_list_keys(CellKey* LCK);
@@ -24,6 +34,8 @@ void delete_cell_key(CellKey* c);
 
 /*Fonction qui supprime une liste chainée de clées LCK*/
 void delete_list_keys(CellKey*LCK);
+
+
 
 
 #endif
