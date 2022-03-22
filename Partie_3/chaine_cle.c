d9589ce8cb7abf462abd06e6234054866a1a4366
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-03-22 18:07:39 +0100

    Update chaine_cle.c

--- Partie_3/chaine_cle.c
+++ Partie_3/chaine_cle.c
@@ -13,3 +13,23 @@ CellKey* ajoutEnTete(Key* key, CellKey* liste){
 	nouv->next = liste;
 	liste = nouv;
 }
+
+CellKey* read_public_keys(char* nom_fichier){
+	File* f = fopen(nom_fichier,"r");
+	if (f==NULL){
+		printf("Erreur d'ouverture du fichier.");
+	}
+	CellKey* liste = NULL;
+	char ligne[256];
+	char key_char[128];
+	char bin[128];
+	while (fgets(ligne, 256,f)!=NULL){
+		if (sscanf(ligne, "%s : %s, %s",bin,key_char,bin) == 3){
+			CellKey* nouv = create_cell_key(str_to_key(key_char));
+			liste = ajoutEnTete(nouv);
+		}else{
+			printf("Erreur de formatage!");
+		}
+	}
+	return liste;
+}
