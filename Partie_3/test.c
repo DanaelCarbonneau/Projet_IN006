efa330dc607395d7c594b9bbad36f7d266714dcc
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-03-31 10:51:36 +0200

    Update test.c

--- Partie_3/test.c
+++ Partie_3/test.c
@@ -11,23 +11,27 @@ de la partie 2 (se référer aux explications précédentes).
 ===============================================================*/
 
 int main(){
-    CellKey* chaine_cle = read_public_keys("../Partie_2/candidates.txt");
-
-   
-    
-    print_list_keys(chaine_cle);
-
-    delete_list_keys(chaine_cle);
-   
+    /*Création des listes chainées selon fichiers de la partie 2*/
+    CellKey* chaine_cle_candidats = read_public_keys("../Partie_2/candidates.txt");
+    CellKey* chaine_cle_votants = read_public_keys("../Partie_2/declarations.txt");
     CellProtected* chaine_pr = read_protected();
     
+    /*Affichage si besoin*/
+    #if 0
+    print_list_keys(chaine_cle_candidats);
+    print_list_protected(chaine_pr);
+    #endif
+
     print_list_protected(chaine_pr);
 
-    chaine_pr = supprimer_fausses_declarations(chaine_pr);
-    
-
+     /*Affichage si besoin*/
+    #if 0
     print_list_protected(chaine_pr);
+    #endif
     
+    /*Liberer la mémoire*/
+    delete_list_keys(chaine_cle_candidats);
+    delete_list_keys(chaine_cle_votants);
     delete_list_protected(chaine_pr);
     
 }
