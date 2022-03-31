36db0081cf0648a86096ded379bc79de8a6fa342
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-03-31 10:57:54 +0200

    Update test.c

--- Partie_3/test.c
+++ Partie_3/test.c
@@ -2,6 +2,10 @@
 #include"chaine_cle.h"
 #include"chaine_protected.h"
 
+/*Taille des tables de hachage*/
+#define SIZE_C 20
+#define SIZE_V 2000
+
 /*==========================ATTENTION==========================
 
 Les fuites mémoires subsitant dans le code de ce test proviennent du même problème que celui énoncé dans le fichier test.c
@@ -14,7 +18,7 @@ int main(){
     /*Création des listes chainées selon fichiers de la partie 2*/
     CellKey* chaine_cle_candidats = read_public_keys("../Partie_2/candidates.txt");
     CellKey* chaine_cle_votants = read_public_keys("../Partie_2/declarations.txt");
-    CellProtected* chaine_pr = read_protected();
+    CellProtected* chaine_pr = read_protected("../Partie_2/declarations.txt");
     
     /*Affichage si besoin*/
     #if 0
@@ -22,14 +26,20 @@ int main(){
     print_list_protected(chaine_pr);
     #endif
 
-    print_list_protected(chaine_pr);
+    supprimer_fausses_declarations(chaine_pr);
 
      /*Affichage si besoin*/
     #if 0
     print_list_protected(chaine_pr);
     #endif
-    
+
+    /*trouver le gagnant de l'élection*/
+    Key* gagnant = compute_winner(chaine_pr, chaine_cle_candidats, chaine_cle_votants, SIZE_C, SIZE_V);
+    char* kts = key_to_str(gagnant);
+    printf("Le gagnant est : %s",kts);
+
     /*Liberer la mémoire*/
+    free(kts);
     delete_list_keys(chaine_cle_candidats);
     delete_list_keys(chaine_cle_votants);
     delete_list_protected(chaine_pr);
