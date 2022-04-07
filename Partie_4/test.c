a7fd74a1debebd36e67a69426cfc1fc35f40ccbe
EleaTriesToCode <98588172+EleaTriesToCode@users.noreply.github.com>
2022-04-07 11:15:30 +0200

    Create test.c

---
+++ Partie_4/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "block.h"
+#include "../Partie_1/gestion_clef.h"
+#include"../Partie_3/chaine_protected.h"
+
+#define D_MAX 10
+
+int main{
+  /*Creer le bloc*/
+  Block* b = (Block*)malloc(sizeof(Block));
+  Key* key_author = (Key*)malloc(sizeof(Key));
+  init_key(key_author,0,0); //on se donne une clé dont on se fiche des valeurs ? 
+  b->author = key_author;
+  b->votes = read_protected("../Partie_2/declarations.txt");
+  b->previous_hash = 0;
+    
+  /*Comparer le temps mis pour lui donner une proof of work de 0 à D_MAX*/
+  generate_fichier_comparaison(b,D_MAX);
+}
+
